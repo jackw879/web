7ade711026bbe7324ed344ea0a2e54221a468260
lidonglovebaby@sina.com <lidong199117>
2014-12-08 20:08:07 +0000

    8/12

--- models/q_comments.js
+++ models/q_comments.js
@@ -1,37 +1,37 @@
 var orm = require('orm');
 
-module.exports=function(orm,db){
-	var q_comment = db.define('q_comment',{
-		title: {type:'text'},
-        contents: {type:'text', big:true},
-        createTime: {type:'date', time:true}
-	},
-	{
-		hooks:{
-			beforeValidation:function(){
-				this.createTime=new Date();
-			}
-		},
-		validations:{
-            title:[orm.enforce.notEmptyString()],
-            contents:[orm.enforce.notEmptyString()]
-		},
-		methods:{
-            serialize: function(){
-             return {
-                    title : this.title,
-                    contents : this.contents,
-                    createTime : this.createTime,
-                }
-            }
+module.exports = function (orm, db) {
+        var q_comment = db.define('q_comment', {
+            title: {type: 'text'},
+            contents: {type: 'text', big: true},
+            createTime: {type: 'date', time: true}
         },
-        autoFetch:true,
-		autoFetchLimit : 2,
-		cache:false	
-	});
-	q_comment.hasOne('question',db.models.question,{reverse:'q_comment',autoFetch:true});
-	
-	q_comment.sync(function (err) {
-		err && console.log(err);
-	});
+            {
+                hooks: {
+                    beforeValidation: function () {
+                        this.createTime = new Date();
+                    }
+                },
+                validations: {
+                    title: [orm.enforce.notEmptyString()],
+                    contents: [orm.enforce.notEmptyString()]
+                },
+                methods: {
+                    serialize: function () {
+                        return {
+                            title : this.title,
+                            contents : this.contents,
+                            createTime : this.createTime
+                        }
+                    }
+                },
+                autoFetch: true,
+                autoFetchLimit : 2,
+                cache: false
+            });
+    q_comment.hasOne('question', db.models.question, {reverse: 'q_comment', autoFetch: true});
+
+    q_comment.sync(function (err) {
+        err && console.log(err);
+    });
 }
