8a6bb94b52f8fe8354fbc1d04190ebb0e038ccdd
jackw879 <jack-webster@hotmail.com>
2014-12-08 15:34:07 +0000

    Update a_comments.js

--- models/a_comments.js
+++ models/a_comments.js
@@ -1,36 +1,36 @@
 var orm = require('orm');
 
-module.exports=function(orm,db){
-	var a_comment = db.define('a_comment',{
-		title: {type:'text'},
-        contents: {type:'text', big:true},
-        createTime: {type:'date', time:true}
-	},
-	{
-		hooks:{
-			beforeValidation:function(){
-				this.createTime=new Date();
-			}
-		},
-		validations:{
-            title:[orm.enforce.notEmptyString()],
-            contents:[orm.enforce.notEmptyString()]
-		},
-		methods:{
-            serialize: function(){
-             return {
-                    title : this.title,
-                    contents : this.contents,
-                    createTime : this.createTime,
-                }
-            }
+module.exports = function (orm, db) {
+        var a_comment = db.define('a_comment', {
+            title: {type: 'text'},
+            contents: {type: 'text', big: true},
+            createTime: {type: 'date', time: true}
         },
-        autoFetch:true,
-		autoFetchLimit : 2,
-		cache:false	
-	});
-	a_comment.hasOne('answer',db.models.answer,{reverse:'a_comment',autoFetch:true});
-	a_comment.sync(function (err) {
-		err && console.log(err);
-	});
+            {
+                hooks: {
+                    beforeValidation: function () {
+                        this.createTime = new Date();
+                    }
+                },
+                validations: {
+                    title: [orm.enforce.notEmptyString()],
+                    contents: [orm.enforce.notEmptyString()]
+                },
+                methods: {
+                    serialize: function () {
+                        return {
+                            title : this.title,
+                            contents : this.contents,
+                            createTime : this.createTime,
+                        }
+                    }
+                },
+                autoFetch: true,
+                autoFetchLimit : 2,
+                cache: false
+            });
+    a_comment.hasOne('answer', db.models.answer, {reverse: 'a_comment', autoFetch: true});
+    a_comment.sync(function (err) {
+        err && console.log(err);
+    });
 }
