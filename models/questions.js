406a28cbc6c3a01e9747013ffbfd5f0eb9a15f14
jackw879 <jack-webster@hotmail.com>
2014-12-08 16:08:38 +0000

    Update questions.js

--- models/questions.js
+++ models/questions.js
@@ -1,35 +1,35 @@
 var orm = require('orm');
 
-module.exports=function(orm,db){
-	var question = db.define('question',{
-		title: {type:'text'},
-        contents: {type:'text', big:true},
-        createTime: {type:'date', time:true}
-	},
-	{
-		hooks:{
-			beforeValidation:function(){
-				this.createTime=new Date();
-			}
-		},
-		validations:{
-            title:[orm.enforce.notEmptyString()],
-            contents:[orm.enforce.notEmptyString()]
-		},
-		methods:{
-            serialize: function(){
-             return {
-                    title : this.title,
-                    contents : this.contents,
-                    createTime : this.createTime,
-                }
-            }
+module.exports = function (orm, db) {
+        var question = db.define('question', {
+            title: {type: 'text'},
+            contents: {type: 'text', big: true},
+            createTime: {type: 'date', time: true}
         },
-        autoFetch:true,
-		autoFetchLimit : 2,
-        cache:false	
-	});
-	question.sync(function (err) {
-		err && console.log(err);
-	});
+            {
+                hooks: {
+                    beforeValidation: function () {
+                        this.createTime = new Date();
+                    }
+                },
+                validations: {
+                    title: [orm.enforce.notEmptyString()],
+                    contents: [orm.enforce.notEmptyString()]
+                },
+                methods: {
+                    serialize: function () {
+                        return {
+                            title : this.title,
+                            contents : this.contents,
+                            createTime : this.createTime
+                        }
+                    }
+                },
+                autoFetch: true,
+                autoFetchLimit : 2,
+                cache: false
+            });
+    question.sync(function (err) {
+        err && console.log(err);
+    });
 }
