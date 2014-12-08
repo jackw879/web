cb72aa9a0d856e87da08500025311c160c226d72
jackw879 <jack-webster@hotmail.com>
2014-12-08 15:50:27 +0000

    Update models.js

--- models/models.js
+++ models/models.js
@@ -1,22 +1,21 @@
-var orm=require('orm');
-var config=require('../config.js');
-var connection=null;
+var orm = require('orm');
+var config = require('../config.js');
+var connection = null;
 
-function setDB(db,cb){
-	require('./questions')(orm,db);
-	require('./answers')(orm,db);
-	require('./q_comments')(orm,db);
-	require('./a_comments')(orm,db);
+function setDB(db, cb) {
+        require('./questions')(orm, db);
+    require('./answers')(orm, db);
+    require('./q_comments')(orm, db);
+    require('./a_comments')(orm, db);
 
-	return cb(null,db);
-	
+    return cb(null, db);
+
 }
 
-module.exports=function(cb){
-	if(connection)
-		return cb(null,connection);
+module.exports = function (cb) {
+         if (connection) return cb(null, connection);
 
-	orm.connect(config.database,function(err,db){
+        orm.connect(config.database,function(err,db){
 		if(err) return cb(err);
 		connection=db;
 		db.settings.set('instance.returnAllErrors', true);
