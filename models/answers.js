96bf6f42358ab2af6dcc39d6afbeeaaa28afb070
jackw879 <jack-webster@hotmail.com>
2014-12-08 15:45:48 +0000

    Update answers.js

--- models/answers.js
+++ models/answers.js
@@ -1,37 +1,36 @@
 var orm = require('orm');
 
-module.exports=function(orm,db){
-	var answer = db.define('answer',{
-		title: {type:'text'},
-        contents: {type:'text', big:true},
-        createTime: {type:'date', time:true}
-	},
-	{
-		hooks:{
-			beforeValidation:function(){
-				this.createTime=new Date();
-			}
-		},
-		validations:{
-            title:[orm.enforce.notEmptyString()],
-            contents:[orm.enforce.notEmptyString()]
-		},
-		methods:{
-            serialize: function(){
-             return {
-                    title : this.title,
-                    contents : this.contents,
-                    createTime : this.createTime,
+module.exports = function (orm, db) {
+        var answer = db.define('answer', {
+        title: {type: 'text'},
+        contents: {type: 'text', big: true},
+        createTime: {type: 'date', time: true}
+    },
+         {
+            hooks: {
+                beforeValidation: function () {
+                    this.createTime = new Date();
                 }
-            }
-        },
-        autoFetch:true,
-		autoFetchLimit : 2,
-		cache:false
-	});
-	//when set autoFetch as true,user can find answer of a question directly
-	answer.hasOne('question',db.models.question,{reverse:'answers',autoFetch:true});
-	answer.sync(function (err) {
-		err && console.log(err);
-	});
+            },
+            validations: {
+                title: [orm.enforce.notEmptyString()],
+                contents: [orm.enforce.notEmptyString()]
+            },
+            methods: {
+                serialize: function () {
+                    return {
+                        title : this.title,
+                        contents : this.contents,
+                        createTime : this.createTime
+                    }
+                }
+            },
+            autoFetch: true,
+            autoFetchLimit : 2,
+            cache: false
+        });
+    answer.hasOne('question', db.models.question, {reverse: 'answers', autoFetch: true});
+    answer.sync(function (err) {
+        err && console.log(err);
+    });
 }
